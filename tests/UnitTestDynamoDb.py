cfb274a7389ec7c21b1c1854193178b074a1d6dc
Assil <bouhache@th-brandenburg.de>
2018-01-24 00:44:00 +0100

    commit

--- tests/UnitTestDynamoDb.py
+++ tests/UnitTestDynamoDb.py
@@ -12,19 +12,19 @@ from sys import getsizeof
 """Test case utilisé pour tester les fonctions du module 'random'."""
 # Helper class to convert a DynamoDB item to JSON.
 
-def test_hashtags():
+def test_table_hashtags():
     """Test le fonctionnement de la table hashtags."""
     dynamodbA = boto3.resource(service_name='dynamodb', region_name='us-west-2')
     tableA = dynamodbA.Table('hashtags')
     responseA = tableA.scan()
-    for recordA in responseA['dada']:
+    for recordA in responseA['Items']:
         testTableA = recordA['hashtag']
     assert (getsizeof(testTableA) > 0)
 
-def test_fullName():
+def test_table_fullName():
     """Test le fonctionnement de la table fullName."""
     dynamodbB = boto3.resource(service_name='dynamodb', region_name='us-west-2')
-    tableB = dynamodbB.Table('bb')
+    tableB = dynamodbB.Table('full_name')
     responseB = tableB.scan()
     for recordB in responseB['Items']:
         testTableB = recordB['full_name']
